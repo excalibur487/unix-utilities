291ddd730154ac362952c6d12ce6d9953a80d5e1
Siddarth <67941820+excalibur487@users.noreply.github.com>
2024-08-06 21:31:27 -0400

    Update zip.c

--- zip.c
+++ zip.c
@@ -4,56 +4,32 @@
 
 int main(int argc, char* argv[]) {
 	if (argc < 2) {
-		printf("wzip: file1 [file2 ...]\n");
+		printf("zip: file1 [file2 ...]\n");
 		exit(1);
 	}
 	FILE *uncomp;
-	int32_t counter = 1;
-	char prev, curr;
-	for (int i = 1; i < argc; i++) {
-		uncomp = fopen(argv[i], "r");
-		int32_t *temp = &counter;
-		if ((prev = curr = fgetc(uncomp)) == EOF) {
-			continue;
-		}
-		while(1) {
-			curr = fgetc(uncomp);
-			if (curr == EOF) {
-				break;
-			}
-			//if (feof(uncomp))
-			//	break;
-			//printf("%d\n", curr);
-			//printf("Count: %d\n", counter);
-			if (curr == '\n') {
-				curr = prev;
-				curr = fgetc(uncomp);
-				if (curr != EOF) {
-					ungetc(curr, uncomp);
-					printf("\n");
+	int32_t i = 1, counter = 1;
+	char buf[2]; 
+	uncomp = fopen(argv[i], "r");
+	while ((buf[0] = fgetc(uncomp)) != EOF) {
+		while (1) {
+			buf[1] = fgetc(uncomp);
+			if (buf[1] == EOF) {
+				if (i >= argc - 1) {
+					fwrite(&counter, 4, 1, stdout); 
+					putc(buf[0], stdout);
+					exit(0);
+				} else {
+					fclose(uncomp);
+					uncomp = fopen(argv[++i], "r");
 				}
-				ungetc(curr, uncomp);
-				curr = prev;
-			}
-			if (curr != prev) {
-				//printf("%d", counter);
-				fwrite(temp, 4, 1, stdout);
-				printf("%c", prev);
-				counter = 0;
-				prev = curr;
-			}
-			counter++;
-		}	
-		//printf("Counter: %d\n", counter);
-		if (i == argc - 1) {
-			counter--;
-			fwrite(temp, 4, 1, stdout);
-			printf("%c", prev);
-			counter = 0;
-			prev = curr;
+			} else if (buf[0] != buf[1]) {
+				fwrite(&counter, 4, 1, stdout);
+				putc(buf[0], stdout);
+				buf[0] = buf[1];
+				counter = 1;
+			} 
+			else counter++;
 		}
-		fclose(uncomp);
-		//printf("ok\n");
 	}
-			
 }
